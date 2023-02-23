f6a83f51215e20c06d85b100a1f50e11739f4da0
Kolman-Freecss <s3rg10.martinez.roman@gmail.com>
2023-02-23 01:03:26 +0100

    Footer OK

--- src/app/shared/components/layout/footer/footer.component.ts
+++ src/app/shared/components/layout/footer/footer.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  linkedin = 'https://www.linkedin.com/in/sergiomartinezroman/';
+  github = 'https://github.com/Kolman-Freecss';
+  youtube = 'https://www.youtube.com/channel/UCZ9Y4Z5QZ5Z9Z5Z9';
+
   constructor() { }
 
   ngOnInit(): void {
