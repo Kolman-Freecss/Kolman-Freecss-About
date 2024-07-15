5690eb1a6d1018c93b1432b8a2ac153764c0f444
Kolman-Freecss <s3rg10.martinez.roman@gmail.com>
2024-07-15 11:18:22 +0200

    Update dependencies compatible with Angular 18

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -5,7 +5,7 @@ import { HomeRoutingModule } from './home.routing';
 import { SharedModule } from '../../shared';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NgxWebstorageModule } from 'ngx-webstorage';
+import { provideNgxWebstorage, withLocalStorage, withNgxWebstorageConfig, withSessionStorage } from 'ngx-webstorage';
 
 
 @NgModule({
@@ -17,9 +17,15 @@ import { NgxWebstorageModule } from 'ngx-webstorage';
       HomeRoutingModule,
       CommonModule,
       FontAwesomeModule,
-      SharedModule,
-      NgxWebstorageModule.forRoot(),
+      SharedModule
    ],
+    providers: [
+        provideNgxWebstorage(
+          withNgxWebstorageConfig({ separator: ':', caseSensitive: true }),
+          withLocalStorage(),
+          withSessionStorage()
+	    )
+    ]
 })
 export class HomeModule {
 }
