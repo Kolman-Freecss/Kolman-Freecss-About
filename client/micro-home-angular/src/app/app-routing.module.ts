e9ef6caa72cef618eb7e39e208506366821baef6
Kolman-Freecss <s3rg10.martinez.roman@gmail.com>
2024-09-06 10:03:33 +0200

    Deploy -> Routes

--- client/micro-home-angular/src/app/app-routing.module.ts
+++ client/micro-home-angular/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { GAMES_PATH, HOME_PATH, ORIGIN_PATH } from './shared/paths';
+import { CONTEXT_PATH, GAMES_PATH, HOME_PATH, ORIGIN_PATH } from './shared/paths';
 import { APP_BASE_HREF } from '@angular/common';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
-import { HomeComponent } from './features/home';
 
 const routes: Routes = [
   { path: '', redirectTo: ORIGIN_PATH + "/" + HOME_PATH, pathMatch: 'full' },
+  { path: ORIGIN_PATH, redirectTo: ORIGIN_PATH + "/" + HOME_PATH, pathMatch: 'full' },
   { path: ORIGIN_PATH + "/" + HOME_PATH, loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
   { path: ORIGIN_PATH + "/" + GAMES_PATH, loadChildren: () => import('./features/game/game.module').then(m => m.GameModule) },
   { path: '**', component: EmptyRouteComponent },
@@ -14,7 +14,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' + CONTEXT_PATH + '/' }],
   exports: [RouterModule],
 })
 export class AppRoutingModule {
--- client/micro-home-angular/src/app/shared/paths.ts
+++ client/micro-home-angular/src/app/shared/paths.ts
@@ -1,4 +1,5 @@
-﻿export const ORIGIN_PATH = 'angular';
+﻿export const CONTEXT_PATH = 'Kolman-Freecss-About';
+export const ORIGIN_PATH = 'angular';
 export const HOME_PATH = 'home';
 export const GAMES_PATH = 'game';
 
