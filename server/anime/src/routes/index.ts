092f450d6cb1f13bb17c7650c804c5044dea4722
Kolman-Freecss <s3rg10.martinez.roman@gmail.com>
2024-09-09 01:55:43 +0200

    First commit NodeJs Express

---
+++ server/anime/src/routes/index.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from 'express';
+
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+export { router as indexRouter };
