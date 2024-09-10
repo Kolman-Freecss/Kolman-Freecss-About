cee6076ebfc01a74abf542e5c7002c61737ff08a
Kolman-Freecss <s3rg10.martinez.roman@gmail.com>
2024-09-10 08:57:41 +0200

    NATS configured

--- server/anime/src/index.ts
+++ server/anime/src/index.ts
@@ -7,36 +7,45 @@ import { natsWrapper } from './events/nats-wrapper';
 config();
 
 const start = async () => {
-  if (!process.env.MONGO_URI) {
-    throw new Error('MONGO_URI must be defined');
-  }
+   if (!process.env.MONGO_URI) {
+      throw new Error('MONGO_URI must be defined');
+   }
+   if (!process.env.NATS_CLIENT_ID) {
+      throw new Error('NATS_CLIENT_ID must be defined');
+   }
+   if (!process.env.NATS_URL) {
+      throw new Error('NATS_URL must be defined');
+   }
+   if (!process.env.NATS_CLUSTER_ID) {
+      throw new Error('NATS_CLUSTER_ID must be defined');
+   }
 
-  try {
-    
-    await natsWrapper.connect(
-      "anime", // Cluster ID previously defined 
-      "abc", // Unique client ID
-      "http://localhost:4222"
-    );
-    natsWrapper.client.on('close', () => {
-      console.log('NATS connection closed!');
-      process.exit();
-    });
-    
-    const mongoUri = process.env.MONGO_URI;
-    await mongoose.connect(mongoUri);
-    console.log('Connected to MongoDb');
-  } catch (err) {
-    console.error(err);
-  }
-  
-  console.log('Starting up listeners...');
-  new PostCreatedListener(natsWrapper.client).listen();
-  console.log('Listeners started!');
+   try {
 
-  app.listen(3000, () => {
-    console.log('Listening on port 3000!!!!!!!!');
-  });
+      await natsWrapper.connect(
+         process.env.NATS_CLUSTER_ID, // Cluster ID previously defined 
+         process.env.NATS_CLIENT_ID, // Unique client ID
+         process.env.NATS_URL,
+      );
+      natsWrapper.client.on('close', () => {
+         console.log('NATS connection closed!');
+         process.exit();
+      });
+
+      const mongoUri = process.env.MONGO_URI;
+      await mongoose.connect(mongoUri);
+      console.log('Connected to MongoDb');
+   } catch (err) {
+      console.error(err);
+   }
+
+   console.log('Starting up listeners...');
+   new PostCreatedListener(natsWrapper.client).listen();
+   console.log('Listeners started!');
+
+   app.listen(3000, () => {
+      console.log('Listening on port 3000!!!!!!!!');
+   });
 };
 
 start();
--- server/posts/src/index.ts
+++ server/posts/src/index.ts
@@ -1,25 +1,42 @@
 import * as mongoose from 'mongoose';
 import app from './app';
 import { config } from 'dotenv';
+import { natsWrapper } from './events/nats-wrapper';
 
 config();
 
 const start = async () => {
-  if (!process.env.MONGO_URI) {
-    throw new Error('MONGO_URI must be defined');
-  }
+   if (!process.env.MONGO_URI) {
+      throw new Error('MONGO_URI must be defined');
+   }
+   if (!process.env.NATS_CLIENT_ID) {
+      throw new Error('NATS_CLIENT_ID must be defined');
+   }
+   if (!process.env.NATS_URL) {
+      throw new Error('NATS_URL must be defined');
+   }
+   if (!process.env.NATS_CLUSTER_ID) {
+      throw new Error('NATS_CLUSTER_ID must be defined');
+   }
 
-  try {
-    const mongoUri = process.env.MONGO_URI;
-    await mongoose.connect(mongoUri);
-    console.log('Connected to MongoDb');
-  } catch (err) {
-    console.error(err);
-  }
+   try {
 
-  app.listen(3001, () => {
-    console.log('Listening on port 3001!!!!!!!!');
-  });
+      await natsWrapper.connect(
+         process.env.NATS_CLUSTER_ID, // Cluster ID previously defined 
+         process.env.NATS_CLIENT_ID, // Unique client ID
+         process.env.NATS_URL,
+      );
+
+      const mongoUri = process.env.MONGO_URI;
+      await mongoose.connect(mongoUri);
+      console.log('Connected to MongoDb');
+   } catch (err) {
+      console.error(err);
+   }
+
+   app.listen(3001, () => {
+      console.log('Listening on port 3001!!!!!!!!');
+   });
 };
 
 start();
